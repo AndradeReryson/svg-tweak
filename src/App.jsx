2f9c8e9939e4dcceedc25e9b99706157cf7f7be7
AndradeReryson <reryson.andrade@fatec.sp.gov.br>
2025-04-07 18:47:18 -0300

    fix: routing issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { useState } from 'react'
 import TelaPrincipal from './Telas/TelaPrincipal/TelaPrincipal'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
 
 export default function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path='/' element={<TelaPrincipal/>} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   )
 }
